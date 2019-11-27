9f16e876118b6c035ba6d48e3e0f73fb4a752029
Ihor <kuryloff@gmail.com>
2019-11-27 18:06:35 +0200

    fixed directives bud

--- angular-calculator/src/app/app.module.ts
+++ angular-calculator/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClickDirDirective } from './directives/click-dir.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClickDirDirective
   ],
   imports: [
     BrowserModule,
